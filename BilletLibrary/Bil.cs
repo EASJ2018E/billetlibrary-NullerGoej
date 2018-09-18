2ce9a1465358054e0ad7bbd9dd0eaf1b68f73ebf
NullerGoej <bastian2010@live.dk>
2018-09-18 21:35:28 +0200

    Tilføjet 2 projekter og Klassen Bil

---
+++ BilletLibrary/Bil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilletLibrary
+{
+    public class Bil
+    {
+        /// <summary>
+        /// Objekt bil.
+        /// </summary>
+        /// <param name="nummerplade"></param>
+        public Bil(string nummerplade)
+        {
+            Nummerplade = nummerplade;
+            Dato = new DateTime();
+        }
+        /// <summary>
+        /// Bilens Nummerplade.
+        /// </summary>
+        public string Nummerplade { get; set; }
+        /// <summary>
+        /// Bilens Dato.
+        /// </summary>
+        public DateTime Dato { get; set; }
+        /// <summary>
+        /// Pris for Bil Billet.
+        /// </summary>
+        /// <returns>Retunere Prisen for Bil Billet</returns>
+        public decimal Pris()
+        {
+            return 240;
+        }
+        /// <summary>
+        /// Type Køretøj.
+        /// </summary>
+        /// <returns>Returene Køretøjet</returns>
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BilletLibrary;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bil bil = new Bil("Nummerplade");
+            Console.WriteLine(bil.Pris());
+
+            Console.ReadLine();
+        }
+    }
+}
